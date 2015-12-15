753ad506ad3ff6110bf03e0a36f301582bf85932
wangxiaoxian <shiaoshian_wong@hotmail.com>
2015-12-15 18:05:47 +0800

    .

--- source/springmvc/src/main/java/cn/springmvc/store/model/PriceChangeLog.java
+++ source/springmvc/src/main/java/cn/springmvc/store/model/PriceChangeLog.java
@@ -14,4 +14,48 @@ package cn.springmvc.store.model;
  */
 public class PriceChangeLog {
 
+	private String priceChangeLogId;
+	private String goodsId;
+	private String price;
+	private String createTime;
+	private String updateTime;
+	private String remark;
+	
+	public String getPriceChangeLogId() {
+		return priceChangeLogId;
+	}
+	public void setPriceChangeLogId(String priceChangeLogId) {
+		this.priceChangeLogId = priceChangeLogId;
+	}
+	public String getGoodsId() {
+		return goodsId;
+	}
+	public void setGoodsId(String goodsId) {
+		this.goodsId = goodsId;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public String getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
 }
--- source/springmvc/src/main/java/cn/springmvc/store/model/User.java
+++ source/springmvc/src/main/java/cn/springmvc/store/model/User.java
@@ -14,4 +14,48 @@ package cn.springmvc.store.model;
  */
 public class User {
 
+	private String userId;
+	private String username;
+	private String password;
+	private String nickname;
+	private String createTime;
+	private String updateTime;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public String getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+	
 }
