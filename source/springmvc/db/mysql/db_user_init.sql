11e481b24d60ee0c6ca6e95271eb008f2c6448f7
wangxiaoxian <wangxiaoxian2@qq.com>
2017-03-24 10:48:47 +0800

    转移代码无关的目录

--- source/springmvc/db/mysql/db_user_init.sql
+++
@@ -1,7 +0,0 @@
-create user 'wxx_store' @ 'localhost' IDENTIFIED by 'wxx_store';
-
--- mysql中schema和database是一样的，所以下面两句话是相同的 
--- create schema wxx_store;
-create database wxx_store;
-
-grant privileges all on wxx_store.* to 'wxx_store' @'localhost';
