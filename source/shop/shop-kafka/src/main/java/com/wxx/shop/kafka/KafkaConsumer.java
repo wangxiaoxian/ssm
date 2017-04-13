5e671990acd72619fa8b052f2043e73671abd6a5
wangxiaoxian <wangxiaoxian2@qq.com>
2017-04-13 18:30:45 +0800

    加入kafka的支持

---
+++ source/shop/shop-kafka/src/main/java/com/wxx/shop/kafka/KafkaConsumer.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2017 Aspire Tech,Inc. All rights reserved.
+ */
+package com.wxx.shop.kafka;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.messaging.Message;
+
+/**
+ * kafka消费者
+ *
+ * @author wangxiaoxian
+ * @version $v:1.0.0, $time:2017-04-13, $id:KafkaConsumer.java, Exp $
+ */
+public class KafkaConsumer {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-consumer.xml");
+        QueueChannel queueChannel = (QueueChannel) context.getBean("inputFromKafka");
+        Message msg = null;
+        while ((msg = queueChannel.receive(-1)) != null) {
+            String map = (String) msg.getPayload();
+            System.out.println(map);
+        }
+    }
+
+}
---
+++ source/shop/shop-kafka/src/main/java/com/wxx/shop/kafka/KafkaProducer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2017 Aspire Tech,Inc. All rights reserved.
+ */
+package com.wxx.shop.kafka;
+
+/**
+ * kafka生产者
+ *
+ * @author wangxiaoxian
+ * @version $v:1.0.0, $time:2017-04-13, $id:KafkaProducer.java, Exp $
+ */
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.GenericMessage;
+
+public class KafkaProducer {
+
+    public static void main(String[] args) {
+        //ApplicationContext context =new ClassPathXmlApplicationContext("spring-produce.xml");
+        final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-produce.xml",
+            KafkaProducer.class);
+        context.start();
+
+        MessageChannel messageChannel = (MessageChannel) context.getBean("inputToKafka");
+        for (int i = 0; i < 15; i++) {
+            Message<String> message = new GenericMessage<String>("test-------------");
+            boolean flag = messageChannel.send(message);
+
+            //boolean flag = messageChannel.send(message, 2 * 1000);
+            System.out.println(flag);
+        }
+
+        try {
+            System.out.println("Thread.sleep===========================");
+            Thread.sleep(100000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        context.close();
+    }
+
+}
